88325872d8b2494e3f4bef7baf76b0a4f0f2f7c5
Sumit Banwakade <sbanwakde4482@gmail.com>
2026-06-26 22:46:30 +0530

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string temp;
+        for(auto s: strs){
+            temp += to_string(s.size());
+            temp += "#";
+            temp += s;
+        }
+        return temp;
+    }
+
+    vector<string> decode(string s) {
+        vector<string>ans;
+        int i=0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j] != '#') j++;
+            int len = stoi(s.substr(i,j-i));
+            j++;
+            ans.push_back(s.substr(j,len));
+            i=j+len;
+        }
+        return ans;
+    }
+};
